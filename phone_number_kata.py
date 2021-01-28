87f9a47a4635b342e98d34aa3180974c440fb434
john-baxter <john.g.baxter@googlemail.com>
2021-01-28 17:05:28 +0000

    Add passing test for input all zeroes. Function returns '(000) 000-0000'.

---
+++ phone_number_kata.py
@@ -0,0 +1,2 @@
+def create_phone_number(n):
+  return "(000) 000-0000"
--- phone_number_kata_test.py
+++ phone_number_kata_test.py
@@ -1,9 +1,17 @@
 import unittest
+from phone_number_kata import create_phone_number
 
 class TestTest(unittest.TestCase):
   def test_setup(self):
     pass
 
+class CreatePhoneNumberTest(unittest.TestCase):
+  def test_input_is_all_zeroes(self):
+    n = [0,0,0,0,0,0,0,0,0,0]
+    expected_result = "(000) 000-0000"
+    actual_result = create_phone_number(n)
+
+    self.assertEqual(actual_result, expected_result)
   
 
 if __name__ == '__main__':
