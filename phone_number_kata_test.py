ac1b1b21fb8a9e24d38fdc6dfa4957f38d663527
john-baxter <john.g.baxter@googlemail.com>
2021-01-28 17:05:55 +0000

    Remove initial setup test.

--- phone_number_kata_test.py
+++ phone_number_kata_test.py
@@ -1,10 +1,6 @@
 import unittest
 from phone_number_kata import create_phone_number
 
-class TestTest(unittest.TestCase):
-  def test_setup(self):
-    pass
-
 class CreatePhoneNumberTest(unittest.TestCase):
   def test_input_is_all_zeroes(self):
     n = [0,0,0,0,0,0,0,0,0,0]
